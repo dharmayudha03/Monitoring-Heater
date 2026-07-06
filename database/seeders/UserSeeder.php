e193c4fd86029111dd3b0e86fab40e3942946d24
dharmayudha <dharmayudha293@gmail.com>
2026-07-06 19:26:31 +0700

    Update seeders to match clean 3 roles and 0 replacements state

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,21 +10,36 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // 1. Super Admin (Jon)
         User::updateOrCreate(
-            ['email' => 'admin@irc.co.id'],
+            ['email' => 'superadmin@irc.co.id'],
             [
-                'name' => 'Administrator System',
-                'role' => 'admin',
-                'password' => Hash::make('admin'),
+                'name' => 'Super Administrator (Jon)',
+                'role' => 'super_admin',
+                'password' => Hash::make('superadmin123'),
+                'plain_password' => 'superadmin123',
             ]
         );
 
+        // 2. Engineering & Maintenance
+        User::updateOrCreate(
+            ['email' => 'engineering@irc.co.id'],
+            [
+                'name' => 'Engineering & Maintenance',
+                'role' => 'engineering',
+                'password' => Hash::make('engineering123'),
+                'plain_password' => 'engineering123',
+            ]
+        );
+
+        // 3. Operator / Public Viewer
         User::updateOrCreate(
             ['email' => 'user@irc.co.id'],
             [
-                'name' => 'Operator Monitoring',
-                'role' => 'operator',
-                'password' => Hash::make('user'),
+                'name' => 'Operator & Public Viewer',
+                'role' => 'user',
+                'password' => Hash::make('user123'),
+                'plain_password' => 'user123',
             ]
         );
     }
